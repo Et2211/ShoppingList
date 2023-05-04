f705eb6eda202397936ce71da6f17eb785967d53
Etienne <e.sharkey@live.com>
2023-05-04 04:17:00 +0100

    Create draggable items on the list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,9 @@ import Item from '../Item/Item';
 import AddItem from '../AddItem/AddItem';
 import { items, addItem } from '../../redux/itemSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import styles from './App.module.scss'
+import { slist } from '../../utilities';
+import { useEffect } from 'react';
 
 function App() {
 
@@ -12,20 +15,31 @@ function App() {
   const shoppingList = useSelector(items)
 
 
+
+
   const addNewItem = (itemName, quantity) => {
     dispatch(addItem({itemName, quantity}))
   }
+  useEffect(()=>{
+    const list = document.getElementById("shoppingList")
+    if (list) slist(list);
+  })
+
 
   return (
     <div className="App">
       <div className='container'>
 
         <h1>Shopping List</h1>
+        <ul id='shoppingList' className='p-0'>
         {shoppingList && shoppingList.map((item, index)=>{
           return ( 
-            <Item item={item} index={index}/>
+            <li className={styles.draggableList}>
+              <Item item={item} index={index}/>
+            </li>
             )
           })}
+          </ul>
 
         <AddItem addItem={addNewItem}/>
       </div>
