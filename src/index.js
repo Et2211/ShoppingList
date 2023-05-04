417ca98316b00d4bbb4693ee93931bf255a5c708
Etienne <e.sharkey@live.com>
2023-05-04 03:47:49 +0100

    Add reedux persist to keep state on refresh

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+
+let persistor = persistStore(store);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+          <App/>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { itemSlice } from './itemSlice'
+import {configureStore} from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {combineReducers} from "redux";
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
 
+import itemSlice from './itemSlice';
 
-export const store = configureStore({
-  reducer: {
-    items: itemSlice.reducer,
+const reducers = combineReducers({
+  items: itemSlice
+});
 
-  },
-})
+const persistConfig = {
+    key: 'root',
+    storage
+};
 
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk]
+});
+
+export default store;
