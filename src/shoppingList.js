9f0dd4561ba20e9ae903eb51ae2309bbbad7d77e
Etienne <e.sharkey@live.com>
2023-05-04 03:04:59 +0100

    Add starting JSON file

---
+++ src/shoppingList.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    "itemName": "Carrots",
+    "quantity": 3,
+    "gotIt" : false
+  },
+  {
+    "itemName": "Bread",
+    "quantity": 2,
+    "gotIt" : false
+  },
+  {
+    "itemName": "Chicken",
+    "quantity": 1,
+    "gotIt" : false
+  },
+  {
+    "itemName": "Coca Cola",
+    "quantity": 1,
+    "gotIt" : false
+  }
+]
