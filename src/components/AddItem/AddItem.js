bf047eaf45e697aa7ae73e06ab325add51ae6d52
Etienne <e.sharkey@live.com>
2023-05-04 04:36:34 +0100

    Mobile styling

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -15,11 +15,11 @@ function AddItem({addItem}) {
   return (
     <>
       <div className='row justify-content-start'>
-        <div className='col-3'>
+        <div className='col-6 col-md-3'>
           <label htmlFor="addItem">Add Item </label>
           <input type="text" className={"form-control " + styles.form_input} id="addItem" name="addItem" value={newItem} onChange={(e)=>setNewItem(e.target.value)}/>
         </div>
-        <div className='col-3'>
+        <div className='col-6 col-md-3'>
           <label for="quantity"> Quantity</label>
           <input type="number" className={"form-control " + styles.form_input} id="quantity" name="quantity" value={quantity} onChange={(e)=>setQuantity(e.target.value)} min="1" max="100"></input>
         </div>
