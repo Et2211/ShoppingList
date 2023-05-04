4e1c378ed65950d1cf6e99f37562a05310b8c853
Etienne <e.sharkey@live.com>
2023-05-04 04:35:32 +0100

    Add pricing to original array

--- src/shoppingList.js
+++ src/shoppingList.js
@@ -2,21 +2,25 @@ export default [
   {
     "itemName": "Carrots",
     "quantity": 3,
-    "gotIt" : false
+    "gotIt" : false,
+    "price" : 4.99
   },
   {
     "itemName": "Bread",
     "quantity": 2,
-    "gotIt" : false
+    "gotIt" : false,
+    "price" : 4.99
   },
   {
     "itemName": "Chicken",
     "quantity": 1,
-    "gotIt" : false
+    "gotIt" : false,
+    "price" : 4.99
   },
   {
     "itemName": "Coca Cola",
     "quantity": 1,
-    "gotIt" : false
+    "gotIt" : false,
+    "price" : 4.99
   }
 ]
