e499a7999f411bd9a1190e08ee335730648e92e1
Etienne <e.sharkey@live.com>
2023-05-04 03:05:35 +0100

    Add ability to remove items and mark as checked

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,4 +1,9 @@
+import { useEffect, useState } from 'react';
 import styles from './Item.module.scss'
+import { removeItem, checkItem } from '../../redux/itemSlice';
+import { useDispatch } from 'react-redux';
+
+
 function Item({item,index}) {
   const dispatch = useDispatch()
 
@@ -7,6 +12,17 @@ function Item({item,index}) {
       <div className='row'>
         <div className={'col ' + styles.item}>
           <p>{item.quantity}x {item.itemName} </p>
+        </div>
+        <div className='col'>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value={item.gotIt} id="flexCheckDefault" onChange={()=>{dispatch(checkItem(index))}}/>
+            <label class="form-check-label" for="flexCheckDefault">Got It</label>
+          </div>
+        </div>
+        <div className='col'>
+          <div onClick={()=>dispatch(removeItem(index))}>
+            <i class="fa-solid fa-xmark"></i>
+          </div>
         </div>
       </div>
     </>
